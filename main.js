d7bacc2c138f0a2ae074e834447b4564a89a5082
Ahmed Mostafa <129973746+AhmedMostafaAi@users.noreply.github.com>
2023-04-18 04:38:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-const toggleSwitch = document.querySelector('#dark-mode-toggle');
+const toggleSwitch = document.querySelector('.toggle');
 const body = document.querySelector('body');
-const darkLabel = document.querySelector('#dark-change');
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === 'dark') {
+  body.classList.toggle('dark-mode');
+  toggleSwitch.checked = true;
+}
 
 toggleSwitch.addEventListener('change', function() {
   if (this.checked) {
     body.classList.add('dark-mode');
-    darkLabel.textContent = 'Light Mode';
+    localStorage.setItem('theme', 'dark');
   } else {
     body.classList.remove('dark-mode');
-    darkLabel.textContent = 'Dark Mode';
+    localStorage.setItem('theme', 'light');
   }    
 });
